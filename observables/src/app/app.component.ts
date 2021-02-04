f2cceb2f683c04c184b8b8eb979ec613d07bde41
Eduardo <edfadeon@gmail.com>
2021-02-03 23:38:41 -0300

    realizando observable

---
+++ observables/src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <div style="text-align:center" class="content">
+      <h1>
+        Welcome to {{title}}!
+      </h1>
+      <span style="display: block">{{ title }} app is running!</span>
+      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    </div>
+    <h2>Here are some links to help you start: </h2>
+    <ul>
+      <li>
+        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
+      </li>
+      <li>
+        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
+      </li>
+      <li>
+        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
+      </li>
+    </ul>
+    
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'observables';
+  
+  minhaObservable(nome: string) : Observable<string>{
+    return new Observable(Subscriber => {
+
+      if(nome === 'Eduardo')
+      {
+        Subscriber.next('pronto'  + nome);
+        Subscriber.next('pronto de novo ?' + nome);
+        setTimeout(() => {
+        Subscriber.next(nome + ' olha o delay' + nome);
+      }, 4000);
+      }
+      else {
+       Subscriber.error('olha o erro ai');   
+      }   
+    });
+  }
+
+  ngOnInit(): void {
+    this.minhaObservable('')
+      .subscribe(result => console.log(result),
+      // tratando o erro
+      erro => console.log(erro));
+
+      // observador
+      const observer = {
+        next: valor => console.log('Next', valor),
+        erro: erro => console.log('Erro', erro),
+        complete: () => console.log('acabou')
+      }
+  }
+}
